d9ff72338aa54442b96e2455657673d3f67e58e3
vincentotieno2 <vincentotieno2099@gmail.com>
2026-02-07 08:16:48 +0300

    New version

--- Python flow control.py
+++ Python flow control.py
@@ -7,3 +7,10 @@ elif marks>=60:
     print("C")
 else:
     print("Fail")
+
+leads=[120,450,90,800]
+qualified=[]
+for lead in leads:
+    if lead>=300:
+        qualified.append(lead)
+    print(qualified)
